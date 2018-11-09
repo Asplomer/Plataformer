9a9fc70616566e5a75da0c4806bd70a151853d6a
asplomer <asplomer@gmail.com>
2018-11-09 12:50:44 -0300

    añadidos comentarios utiles

--- Plataformer/Plataformer/main.cpp
+++ Plataformer/Plataformer/main.cpp
@@ -30,3 +30,39 @@ int main()
 
 	return 0;
 }
+
+/*
+https://www.sfml-dev.org/tutorials/2.5/graphics-transform.php
+
+basic code for later
+movement:
+entity.setPosition(10.0f, 50.0f);
+entity.move(5.0f, 5.0f);
+sf::Vector2f position = entity.getPosition();
+
+origin of the sprite:
+entity.setOrigin(10.f, 20.f);
+sf::Vector2f origin = entity.getOrigin(); // = (10, 20)
+
+bounding box y colisiones:
+
+// get the bounding box of the entity
+sf::FloatRect boundingBox = entity.getGlobalBounds();
+
+// check collision with a point
+sf::Vector2f point = ...;
+if (boundingBox.contains(point))
+{
+// collision!
+}
+
+// check collision with another box (like the bounding box of another entity)
+sf::FloatRect otherBox = ...;
+if (boundingBox.intersects(otherBox))
+{
+// collision!
+}
+
+textures:
+https://www.sfml-dev.org/tutorials/2.5/graphics-sprite.php
+*/
